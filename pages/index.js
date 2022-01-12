1865a6b41c888e4eb512efd946eb0729ea70eb0c
jamesklein218 <jamesklein218@gmail.com>
2022-01-12 20:10:25 +0700

    First try
    - Load 1000 images with brutforce looping method
    - 62% Performance, 80% SEO

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
+import axios from 'axios'
+import Image from 'next/image';
+
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await axios.get("https://randomuser.me/api/?results=1000")
+  console.log(res.data.results);
+  return {
+    props: {
+      people: res.data.results,
+    }
+  }
+}
+
+
+export default function Home({ people }) {
   return (
     <div className={styles.container}>
-      
+      {people.map(person => {
+        return <Image
+        key={person.id.name}
+        src={person.picture.large}
+        width="250px"
+        height="250px"
+        alt={person.name.title + " " + person.name.first + " " + person.name.last} 
+        />
+      })}
     </div>
   )
 }
+
+
